0eb75e65dcf7b61a079798c9cd62b9ba4ba32adf
Abdullo Abdurazzakov <abdulloabdurazzakov@MacBook-Air-Abdullo.local>
2026-05-14 23:23:02 +0500

    add to requirements.txt

--- script.py
+++ script.py
@@ -2,11 +2,14 @@ import requests
 import time
 
 URL = "https://translate-xqr9.onrender.com"
+URL = "https://translate-xqr9.onrender.com"
 
 while True:
     try:
         response = requests.get(URL)
+        response1 = requests.get(URL)
         print(f"Pinged: {response.status_code}")
+        print(f"Pinged: {response1.status_code}")
     except Exception as e:
         print("Error:", e)
 
