b0b67769515d2af72a76d8666da1c71d81df5a0d
Abdullo Abdurazzakov <abdulloabdurazzakov@MacBook-Air-Abdullo.local>
2026-05-14 23:52:15 +0500

    add to script.py

--- script.py
+++ script.py
@@ -1,16 +1,17 @@
 import requests
 import time
 
-URL = "https://translate-1-nx5c.onrender.com"
-URL = "https://translate-run-script-13af.onrender.com"
+URLS = [
+    "https://translate-1-nx5c.onrender.com",
+    "https://translate-run-script-13af.onrender.com"
+]
 
 while True:
-    try:
-        response = requests.get(URL)
-        response1 = requests.get(URL)
-        print(f"Pinged: {response.status_code}")
-        print(f"Pinged: {response1.status_code}")
-    except Exception as e:
-        print("Error:", e)
+    for url in URLS:
+        try:
+            response = requests.get(url, timeout=10)
+            print(f"Pinged {url}: {response.status_code}")
+        except Exception as e:
+            print(f"Error {url}: {e}")
 
-    time.sleep(300)  # 5 minut
+    time.sleep(300)
